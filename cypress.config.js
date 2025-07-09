4643789fc97e6af976d2c5d29cd57ab0b9237a82
Leandro da Costa Barboza <33236377+lSpenserl@users.noreply.github.com>
2025-07-09 20:36:55 -0300

    utilizando mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,14 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+      video: true,
+      reporter:'mochawesome',
+      reporterOptions: {
+        reportDir:'cypress/result',
+        overwrite: true,
+        html: true,
+        json: false,
+        timestamp: "yyyymmdd_HHMMss"
+      }
   },
-  video: true,
 });
